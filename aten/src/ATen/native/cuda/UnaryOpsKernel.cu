669bc8290912fe4736717a69f6e540d872533b8a
Hong Xu <hong@topbug.net>
2019-08-01 16:09:40 -0700

    [WIP] Support abs for half on CUDA and bool on both CPU and CUDA.
    ghstack-source-id: 128da4cb518c04db288936272d5296cc4e64db34
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/23579

--- aten/src/ATen/native/cuda/UnaryOpsKernel.cu
+++ aten/src/ATen/native/cuda/UnaryOpsKernel.cu
@@ -9,6 +9,26 @@
 
 namespace at { namespace native {
 
+void abs_kernel_cuda(TensorIterator& iter) {
+  switch (iter.dtype()) {
+    case ScalarType::Bool:
+      gpu_kernel(iter, []GPU_LAMBDA(bool a) { return a; });
+      break;
+    case ScalarType::Half:
+      gpu_kernel(iter, []GPU_LAMBDA(at::Half a) -> at::Half {
+        at::Half nega = -a;
+        return a >= nega ? a : nega;
+      });
+      break;
+    default:
+      AT_DISPATCH_ALL_TYPES(iter.dtype(), "abs_cuda", [&]() {
+        gpu_kernel(iter, []GPU_LAMBDA(scalar_t a) -> scalar_t {
+          return std::abs(a);
+        });
+      });
+  }
+}
+
 void bitwise_not_kernel_cuda(TensorIterator& iter) {
   if (iter.dtype() == ScalarType::Bool) {
     gpu_kernel(iter, []GPU_LAMBDA(bool a) {
@@ -23,6 +43,7 @@ void bitwise_not_kernel_cuda(TensorIterator& iter) {
   }
 }
 
+REGISTER_DISPATCH(abs_stub, &abs_kernel_cuda);
 REGISTER_DISPATCH(bitwise_not_stub, &bitwise_not_kernel_cuda);
 
 }}
