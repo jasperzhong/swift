a8a10541a4a69b3e04280364ca414363c021dd2f
Edward Z. Yang <ezyang@fb.com>
2019-08-05 13:55:14 -0400

    [DO NOT MERGE] Temporarily use pr/nightly-rename builder branch [skip ci]
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    ghstack-source-id: 755b7eb850776b26e2c8e91d7a23163f07c14099
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/23765

--- .circleci/scripts/binary_checkout.sh
+++ .circleci/scripts/binary_checkout.sh
@@ -39,10 +39,8 @@ git --no-pager log --max-count 1
 popd
 
 # Clone the Builder master repo
-git clone -q https://github.com/pytorch/builder.git "$BUILDER_ROOT"
+git clone -q https://github.com/pytorch/builder.git "$BUILDER_ROOT" -b pr/nightly-rename
 pushd "$BUILDER_ROOT"
-git fetch origin
-git reset origin/master --hard
 echo "Using builder from "
 git --no-pager log --max-count 1
 popd
