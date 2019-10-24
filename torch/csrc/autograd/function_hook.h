b6a108a7e4ca265708ff611776010e82ab2c44a5
Edward Z. Yang <ezyang@fb.com>
2019-10-24 10:32:43 -0400

    Merge Tensor and Variable types.
    This PR eliminates the static (but not dynamic) distinction between
    Tensor and Variable.  Every Variable is a Tensor, no need to static_cast
    or call the Variable constructor.  The dynamic distinction will be eliminated
    in a later diff.
    
    To do this, I need Tensor to have API parity with Variable.  Thanks
    to the efforts of Will Feng and others, most of the hard work has
    already been done; I just dump all public methods on Variable into
    Tensor.  After doing this, there a few places the implementations
    migrate:
    
    - Some previously inline implementations only reference TensorImpl.
      This can be placed inline in TensorBody.h
    - Some previously inline implementations reference AutogradMeta.
      For the time being, AutogradMeta continues to live in variable.h;
      thus, these implementations must move to be out-of-line,
      in Tensor.cpp
    - However, there are also some template methods.  Those methods are
      retained variable.h
    - Some previous implementations are defined in native_functions.yaml.
      In this case, I don't define them explicitly in Tensor; instead
      they are placed in VariableTypeManual.cpp.  When I did this, I would
      have deleted documentation; instead, this documentation was moved
      to native_functions.yaml
    - All out-of-line implementations that don't fall under the previous
      category get put in Tensor.cpp.
    - Private inline methods got turned into non-method helper functions.
      There was only one of these, _create_cpp_hook
    
    I have to add a number of new forward declarations (and sometimes not
    forward declarations) to Tensor.h.
    
    One API difference is that all Variable methods now have const, so we no longer
    have faux const-correctness (see https://github.com/zdevito/ATen/issues/27 for
    back story)
    
    I would have preferred to eliminate the dynamic distinction first,
    but I wanted inline access to AutogradMeta in Tensor, and the
    AutogradMeta struct references Variable (furthermore, I cannot
    make it reference Tensor, as we return Variable by mutable reference
    from grad() to support the "x.grad() = ..." idiom).
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    ghstack-source-id: ebf5f229c7c2bae2bd018331fa2c15d8ad3add9c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/28287

--- torch/csrc/autograd/function_hook.h
+++ torch/csrc/autograd/function_hook.h
@@ -1,25 +1,9 @@
 #pragma once
 
-#include <vector>
-#include <torch/csrc/WindowsTorchApiMacro.h>
-
-// A hook that's called on gradients
+#include <ATen/Tensor.h>
 
 namespace torch { namespace autograd {
 
-struct Variable;
-using variable_list = std::vector<Variable>;
+using variable_list = std::vector<at::Tensor>;
 
-struct TORCH_API FunctionPreHook {
-  virtual ~FunctionPreHook();
-  virtual variable_list operator()(const variable_list& grads) = 0;
-};
-
-struct TORCH_API FunctionPostHook {
-  virtual ~FunctionPostHook();
-  virtual variable_list operator()(
-    const variable_list& outputs /* grad_inputs */,
-    const variable_list& inputs /* grad_outputs */) = 0;
-};
-
-}} // namespace torch::autograd
+}}
