c798f5fd55b31391d21f24e5c5aa35069c8b9e33
Mike Ruberry <mruberry@devfair044.maas>
2019-10-24 22:47:01 -0700

    two additional benchmarks

--- proto/hash_benchmark.cpp
+++ proto/hash_benchmark.cpp
@@ -1,5 +1,9 @@
 #include <torch/torch.h>
 
+#include <ATen/ATen.h>
+#include <ATen/core/functional.h> // fmap
+#include <torch/csrc/jit/fuser/tensor_desc.h>
+
 #include <cstdint>
 #include <chrono>
 #include <iostream>
@@ -7,15 +11,19 @@
 typedef std::chrono::nanoseconds ns;
 constexpr uint64_t iters = 100000;
 
+using torch::jit::fuser::TensorDesc;
+
 int main() {
 
   auto a = torch::ones({2, 3, 4});
   auto b = torch::ones({3, 4});
+  std::vector<torch::Tensor> inputs{a, b};
+  std::vector<TensorDesc> descs = c10::fmap<TensorDesc>(inputs);
 
   const auto start = std::chrono::high_resolution_clock::now();
 
   for (auto i = decltype(iters){0}; i < iters; ++i) {
-    auto c = a + b;
+    const size_t hash_code = torch::get_hash(0, inputs.size(), inputs);
   }
 
   const auto end = std::chrono::high_resolution_clock::now();
