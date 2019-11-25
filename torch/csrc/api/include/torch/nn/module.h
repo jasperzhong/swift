xiaomaAI123/pytorch

Use torch::OrderedDict instead of std::vector to store hooks for Variable
gh-metadata: pytorch pytorch 30279 gh/yf225/82/head

torch/csrc/api/include/torch/nn/module.h
@@ -1,7 +1,7 @@
 #pragma once
 
+#include <torch/csrc/ordered_dict.h>
 #include <torch/nn/pimpl.h>
-#include <torch/ordered_dict.h>
 #include <torch/serialize/archive.h>
 #include <torch/types.h>
 

torch/csrc/api/include/torch/python.h
@@ -2,12 +2,12 @@
 
 #include <torch/detail/static.h>
 #include <torch/nn/module.h>
-#include <torch/ordered_dict.h>
 #include <torch/types.h>
 
 #include <torch/csrc/Device.h>
 #include <torch/csrc/Dtype.h>
 #include <torch/csrc/DynamicTypes.h>
+#include <torch/csrc/ordered_dict.h>
 #include <torch/csrc/python_headers.h>
 #include <torch/csrc/utils/pybind.h>
 

torch/csrc/autograd/variable.h
@@ -184,7 +184,7 @@ struct TORCH_API AutogradMeta : public c10::AutogradMetaInterface {
   std::weak_ptr<Node> grad_accumulator_;
 
   std::vector<std::shared_ptr<FunctionPreHook>> hooks_;
-  std::shared_ptr<hooks_list> cpp_hooks_list;
+  std::shared_ptr<hooks_map> cpp_hooks_map;
 
   // Only meaningful on leaf variables (must be false otherwise)
   bool requires_grad_;

torch/csrc/jit/script/module.h
@@ -10,7 +10,7 @@
 #include <torch/csrc/jit/source_range.h>
 
 #include <torch/csrc/WindowsTorchApiMacro.h>
-#include <torch/csrc/api/include/torch/ordered_dict.h>
+#include <torch/csrc/ordered_dict.h>
 #include <torch/csrc/jit/script/compilation_unit.h>
 #include <torch/csrc/utils/memory.h>
 

jasperzhong/swift

torch/csrc/api/include/torch/nn/module.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <torch/nn/pimpl.h>
-#include <torch/ordered_dict.h>
+#include <c10/util/ordered_dict.h>
 #include <torch/serialize/archive.h>
 #include <torch/types.h>
 

torch/csrc/api/include/torch/python.h
@@ -2,7 +2,7 @@
 
 #include <torch/detail/static.h>
 #include <torch/nn/module.h>
-#include <torch/ordered_dict.h>
+#include <c10/util/ordered_dict.h>
 #include <torch/types.h>
 
 #include <torch/csrc/Device.h>

torch/csrc/autograd/variable.h
@@ -184,7 +184,7 @@ struct TORCH_API AutogradMeta : public c10::AutogradMetaInterface {
   std::weak_ptr<Node> grad_accumulator_;
 
   std::vector<std::shared_ptr<FunctionPreHook>> hooks_;
-  std::shared_ptr<hooks_list> cpp_hooks_list;
+  std::shared_ptr<hooks_dict> cpp_hooks_dict;
 
   // Only meaningful on leaf variables (must be false otherwise)
   bool requires_grad_;

torch/csrc/jit/script/module.h
@@ -10,7 +10,7 @@
 #include <torch/csrc/jit/source_range.h>
 
 #include <torch/csrc/WindowsTorchApiMacro.h>
-#include <torch/csrc/api/include/torch/ordered_dict.h>
+#include <c10/util/ordered_dict.h>
 #include <torch/csrc/jit/script/compilation_unit.h>
 #include <torch/csrc/utils/memory.h>
 