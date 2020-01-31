759d2b413913148f9047445535b657d74cc3d889
Will Feng <willfeng@fb.com>
2020-01-31 18:31:09 -0500

    DEBUG comment

--- aten/src/ATen/templates/TensorBody.h
+++ aten/src/ATen/templates/TensorBody.h
@@ -402,6 +402,7 @@ class CAFFE2_API Tensor {
   Tensor index(ArrayRef<TensorIndex> indices) const;
   Tensor index(std::initializer_list<TensorIndex> indices) const;
 
+  // yf225 TODO: we need to add `bool accumulate = false` as argument
   Tensor & index_put_(std::initializer_list<Tensor> indices, Tensor const & rhs);
   Tensor & index_put_(ArrayRef<TensorIndex> indices, Tensor const & rhs);
   Tensor & index_put_(ArrayRef<TensorIndex> indices, Tensor && rhs);
