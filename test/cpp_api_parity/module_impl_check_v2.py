e075f43df0aba2439177253197da6042e9f92c9f
Will Feng <willfeng@fb.com>
2020-03-20 02:05:10 -0400

    wip

--- test/cpp_api_parity/module_impl_check_v2.py
+++ test/cpp_api_parity/module_impl_check_v2.py
@@ -171,7 +171,7 @@ def test_forward_backward(unit_test_class, test_params):
   arg_dict_module = torch.jit.ScriptModule()
   for arg_name, arg_value in arg_dict_flat.items():
     assert isinstance(arg_value, torch.Tensor)
-    arg_dict_module._c._register_buffer(arg_name, arg_value)
+    arg_dict_module.register_buffer(arg_name, arg_value)
   arg_dict_module.save("{}/{}_arg_dict.pt".format(test_params.cpp_tmp_folder, module_variant_name))
 
   cpp_test_name = '{}_{}'.format(test_params.module_variant_name, 'test_forward_backward')
