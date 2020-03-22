5e1859b4bb275a57b836312d58e0e1efecac2aa6
Will Feng <willfeng@fb.com>
2020-03-22 13:46:28 -0400

    Turn on C++ API parity test again
    gh-metadata: pytorch pytorch 35190 gh/yf225/106/head

---
+++ test/cpp_api_parity/torch_nn_functionals.py
@@ -0,0 +1,11 @@
+from collections import namedtuple
+
+TorchNNFunctionalMetadata = namedtuple(
+    'TorchNNFunctionalMetadata',
+    [
+        'cpp_sources',
+    ]
+)
+TorchNNFunctionalMetadata.__new__.__defaults__ = ('',)
+
+functional_metadata_map = {}
