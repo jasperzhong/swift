1b1a739c579c31735d43892b3bdb02827236f248
myuan <myuan@fb.com>
2020-04-09 09:49:15 -0700

    [Lite Interpreter] Remove register_mobile_ops.cpp and dummy placeholder for code-gen
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/36050
    
    1. Remove register_mobile_ops.cpp. Bring back register_special_ops.cpp to cover the ops that were in register_mobile_ops.
    2. Remove dummy placeholder. If an op is not in the whitelist, simple don't generate it. It reduces around 200k build size.
    3. To be consistent to aten selective build, skip overload name for code-gen. All the overloaded operators will be generated. Later we could add back the overload name for finer granularity and reduce the size further.
    ghstack-source-id: 101851350
    
    Differential Revision: [D20787026](https://our.internmc.facebook.com/intern/diff/D20787026/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D20787026/)!

--- tools/jit/gen_jit_dispatch.py
+++ tools/jit/gen_jit_dispatch.py
@@ -285,6 +285,7 @@ def gen_jit_dispatch(
     disable_autograd=False,
     selected_op_list_path=None,
     selected_op_list=None,
+    force_schema_registration=False,
 ):
     REGISTER_ATEN_OPS_CPP = CodeTemplate.from_file(template_path + '/register_aten_ops.cpp')
 
@@ -362,13 +363,17 @@ def gen_jit_dispatch(
                                              lvalues=lvalues)
         return constructor
 
-    def filter_decls(jit_decls, disable_autograd, selected_op_list):
+    def filter_decls(jit_decls, disable_autograd, selected_op_list, force_schema_registration):
         result = []
         for decl in jit_decls:
             if disable_autograd and is_backward_op(decl):
                 continue
-            if selected_op_list and signature_without_args(decl) not in selected_op_list:
-                decl['emit_dummy_placeholder'] = True
+            op_name = signature_without_args(decl)
+            if selected_op_list and op_name not in selected_op_list:
+                if force_schema_registration:
+                    decl['emit_dummy_placeholder'] = True
+                else:
+                    continue
             result.append(decl)
         return result
 
@@ -462,7 +467,7 @@ def gen_jit_dispatch(
     if not selected_op_list:
         selected_op_list = []
     selected_op_list += load_op_list(selected_op_list_path) if selected_op_list_path else []
-    jit_decls = filter_decls(jit_decls, disable_autograd, selected_op_list)
+    jit_decls = filter_decls(jit_decls, disable_autograd, selected_op_list, force_schema_registration)
 
     # generation is deterministic
     jit_decl_groups = sort_decls(jit_decls)
