8ba772f83d0169ac18999845e890f5ea22eec7c1
Eli Uriegas <eliuriegas@fb.com>
2020-09-10 15:34:05 -0700

    packaging: Add wheel packaging scripts
    Moves the wheel packaging scripts from their previous home in
    `pytorch/builder` to a place in the main repository.
    
    Should make it easier to test any changes we might need to make to these
    scripts.
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>
    
    ghstack-source-id: cc13313ab24f190c155bee55c511cf542fa2f1b5
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/44501
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- .circleci/scripts/binary_linux_build.sh
+++ .circleci/scripts/binary_linux_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+GIT_ROOT_DIR=$(git rev-parse --show-toplevel)
+
 echo "RUNNING ON $(uname -a) WITH $(nproc) CPUS AND $(free -m)"
 set -eux -o pipefail
 source /env
@@ -9,16 +11,34 @@ source /env
 # But ncpu will return total number of cores on the system
 export MAX_JOBS=18
 
+USE_CUDA=${USE_CUDA:-1}
+if [[ "${DESIRED_CUDA}" == *"cpu"* ]]; then
+  USE_CUDA="0"
+fi
+
 # Parse the parameters
 if [[ "$PACKAGE_TYPE" == 'conda' ]]; then
-  build_script='conda/build_pytorch.sh'
-elif [[ "$DESIRED_CUDA" == cpu ]]; then
-  build_script='manywheel/build_cpu.sh'
+  build_script='/builder/conda/build_pytorch.sh'
 elif [[ "$DESIRED_CUDA" == *"rocm"* ]]; then
-  build_script='manywheel/build_rocm.sh'
+  build_script='/builder/manywheel/build_rocm.sh'
 else
-  build_script='manywheel/build.sh'
+  if [[ -n "$DESIRED_PYTHON" && "$DESIRED_PYTHON" != cp* ]]; then
+      if [[ "$DESIRED_PYTHON" == '2.7mu' ]]; then
+        DESIRED_PYTHON='cp27-cp27mu'
+      elif [[ "$DESIRED_PYTHON" == '3.8m' ]]; then
+        DESIRED_PYTHON='cp38-cp38'
+      else
+        python_nodot="$(echo $DESIRED_PYTHON | tr -d m.u)"
+        DESIRED_PYTHON="cp${python_nodot}-cp${python_nodot}m"
+      fi
+  fi
+  py_majmin="${DESIRED_PYTHON:2:1}.${DESIRED_PYTHON:3:1}"
+  pydir="/opt/python/$DESIRED_PYTHON"
+  export PATH="$pydir/bin:$PATH"
+  git submodule update --init --recursive
+  pip install -q -r requirements.txt
+  build_script="${GIT_ROOT_DIR}/packaging/wheel/build.sh"
 fi
 
 # Build the package
-SKIP_ALL_TESTS=1 stdbuf -i0 -o0 -e0 "/builder/$build_script"
+USE_CUDA="${USE_CUDA}" SKIP_ALL_TESTS=1 stdbuf -i0 -o0 -e0 "$build_script"
