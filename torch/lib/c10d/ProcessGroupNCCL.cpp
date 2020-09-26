38d2fc7707e43b28c77c8b5290b5a76c7125d390
mingzhe0908 <mingzhe0908@devgpu022.atn3.facebook.com>
2020-09-25 17:06:33 -0700

    [NCCL][Test Only] no change
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/45320
    
    
    ghstack-source-id: 112964322
    
    Differential Revision: [D23922690](https://our.internmc.facebook.com/intern/diff/D23922690/)

--- torch/lib/c10d/ProcessGroupNCCL.cpp
+++ torch/lib/c10d/ProcessGroupNCCL.cpp
@@ -205,6 +205,7 @@ ncclResult_t ncclAlltoallv(
     ncclDataType_t type,
     ncclComm_t comm,
     cudaStream_t stream) {
+  std::cout << "ncclAlltoallv?" << std::endl;
   int numranks;
   C10D_NCCL_CHECK(ncclCommCount(comm, &numranks));
   C10D_NCCL_CHECK(ncclGroupStart());
