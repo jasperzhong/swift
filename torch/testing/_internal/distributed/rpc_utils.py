f0a49013fe123b66c84c9f92ae329a2ec7cd5660
Luca Wehrstedt <lcw@fb.com>
2020-09-29 04:08:58 -0700

    [RPC tests] Print verbose logging for TensorPipe
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/45440
    
    Some of our tests are flaky but when they fail they print very little information which makes it hard to diagnose the issue. One very useful piece of information which we'd love to have for the TensorPipe agent is its verbose logs. They are divided into many levels and printing all of them is probably quite impractical as that would create a bloat in the logs and a slowdown of the tests. A hopefully decent compromise is to only print the first level, which gives details about the interface between TensorPipe and PyTorch: which methods of TensorPipe are called by PyTorch, and which callbacks are invoked. This should hopefully not be too big, and would at least help "bisect" the issue into whether it's in TensorPipe or in the agent.
    ghstack-source-id: 113114956
    
    Differential Revision: [D23963480](https://our.internmc.facebook.com/intern/diff/D23963480/)

--- torch/testing/_internal/distributed/rpc_utils.py
+++ torch/testing/_internal/distributed/rpc_utils.py
@@ -53,7 +53,7 @@ from torch.testing._internal.distributed.rpc.rpc_test import (
 # the agent, which then gets mixed-in with each test suite and each mp method.
 
 
-class RpcMultiProcessTestCase(MultiProcessTestCase):
+class RpcMultiProcessTestCase(MultiProcessTestCase, RpcAgentTestFixture):
     def setUp(self):
         super().setUp()
         self.output_dir = tempfile.TemporaryDirectory()
@@ -71,6 +71,10 @@ class RpcMultiProcessTestCase(MultiProcessTestCase):
         ) as f:
             os.dup2(f.fileno(), 2)
         sys.stderr = os.fdopen(2, "wt")
+
+    def subprocess_env_vars(self):
+        env_vars = self.get_env_vars()
+        return env_vars
 
     def on_test_failure(self):
         for rank in range(self.world_size):
