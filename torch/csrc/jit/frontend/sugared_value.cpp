29a180adaaebab1a9991f998233865cded566be9
Wanchao Liang <wanchaol@users.noreply.github.com>
2020-10-26 21:46:15 -0700

    [torchbind] add static define for C++ method
    ghstack-source-id: 4c39f06d1c38b04ded303aa2a40606652d320842
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/44044

--- torch/csrc/jit/frontend/sugared_value.cpp
+++ torch/csrc/jit/frontend/sugared_value.cpp
@@ -611,11 +611,15 @@ std::shared_ptr<SugaredValue> ClassValue::attr(
     const SourceRange& loc,
     Function& m,
     const std::string& field) {
-  if (field != "__new__") {
+
+  if (field == "__new__") {
+    return SpecialFormValue::create(prim::CreateObject);
+  } else if(auto func = type_->findMethod(field)) {
+    return std::make_shared<FunctionValue>(func);
+  } else {
     throw ErrorReport(loc) << "Tried to lookup unknown attribute on class "
                            << type_->annotation_str();
   }
-  return SpecialFormValue::create(prim::CreateObject);
 }
 
 std::shared_ptr<SugaredValue> NamedTupleConstructor::call(
