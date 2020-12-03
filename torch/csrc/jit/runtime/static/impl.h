d2ae9c7bd080285d5af8c0243a0237fdf002c1db
Scott Wolchok <swolchok@fb.com>
2020-12-03 10:38:48 -0800

    [PyTorch] Save some space in ProcessedNode
    `std::function` already has an empty state; no need to wrap
    it in `c10::Optional`.
    
    Differential Revision: [D25296912](https://our.internmc.facebook.com/intern/diff/D25296912/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D25296912/)!
    
    [ghstack-poisoned]

--- torch/csrc/jit/runtime/static/impl.h
+++ torch/csrc/jit/runtime/static/impl.h
@@ -262,7 +262,7 @@ class ProcessedNode {
   }
 
   bool has_out_variant() const {
-    return fn_.has_value();
+    return static_cast<bool>(fn_);
   }
 
   const std::vector<size_t>& input_regs() const {
@@ -281,9 +281,9 @@ class ProcessedNode {
  private:
   Node* node_;
   c10::optional<Operation> op_;
-  c10::optional<std::function<void(const ProcessedNode*, std::vector<IValue>&)>>
+  std::function<void(const ProcessedNode*, std::vector<IValue>&)>
       fn_;
-  c10::optional<std::function<void(const ProcessedNode*, std::vector<IValue>&)>>
+  std::function<void(const ProcessedNode*, std::vector<IValue>&)>
       native_fn_;
 
   std::vector<size_t> input_regs_;
