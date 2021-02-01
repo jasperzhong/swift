26aefc8fd2e91a8d1f4ab6af5bfce829e39f1763
Brian Hirsh <hirsheybar@fb.com>
2021-02-01 10:38:05 -0800

    [WIP] POC of plumbing TLS keys through the dispatcher
    ghstack-source-id: 92d02e289af3a740c528eb5bd12aad97e43718f9
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/49354

--- aten/src/ATen/core/boxing/impl/boxing.h
+++ aten/src/ATen/core/boxing/impl/boxing.h
@@ -149,11 +149,14 @@ struct BoxedKernelWrapper {
   // template parameters in the expression, e.g. FuncType here. However, since
   // `sizeof(FuncType) != sizeof(FuncType)` is always false, this has the same
   // effect.
-  static_assert(sizeof(FuncType) != sizeof(FuncType),
-    "Function signature contains one or more unsupported parameter and/or return types. "
-    "Look for a nearby error like "
-    "\"'call' is not a member of 'c10::impl::BoxedKernelWrapper<(your function type), void>'\" "
-    "- (your function type) is the unsupported signature.");
+
+  static_assert(can_box<IntArrayRef>::value, "FAIL2. can't box IntArrayRef");
+  static_assert(can_box<c10::optional<MemoryFormat>>::value, "FAIL2. can't box c10::optional<MemoryFormat>");
+  static_assert(can_box<const at::Tensor &>::value, "FAIL2. can't box const Tensor &");
+  static_assert(can_box<bool>::value, "FAIL2. can't box bool");
+
+  static_assert(can_box_all<IntArrayRef, c10::optional<MemoryFormat>>::value, "FAIL2. can't box IntArrayRef and c10::optional<MemoryFormat>");
+  static_assert(can_box_all<const at::Tensor &, bool>::value, "FAIL2. can't box const Tensor & and bool");
 };
 
 //
@@ -173,10 +176,11 @@ struct BoxedKernelWrapper<
     KernelFunction::InternalBoxedKernelFunction* boxed_kernel_func,
     OperatorKernel* functor,
     const OperatorHandle& opHandle,
+    DispatchKeySet dispatchKeySet,
     Args... args
   ) {
     torch::jit::Stack stack = boxArgs(args...);
-    (*boxed_kernel_func)(functor, opHandle, &stack);
+    (*boxed_kernel_func)(functor, opHandle, dispatchKeySet, &stack);
 
     return guts::if_constexpr<!std::is_same<void, Result>::value>(
       [&] (auto delay_check) {
@@ -204,6 +208,14 @@ struct BoxedKernelWrapper<
 // return the in-place argument.
 //
 
+  static_assert(can_box<IntArrayRef>::value, "FAIL. can't box IntArrayRef");
+  static_assert(can_box<c10::optional<MemoryFormat>>::value, "FAIL. can't box c10::optional<MemoryFormat>");
+  static_assert(can_box<const at::Tensor &>::value, "FAIL. can't box const Tensor &");
+  static_assert(can_box<bool>::value, "FAIL. can't box bool");
+
+  static_assert(can_box_all<IntArrayRef, c10::optional<MemoryFormat>>::value, "FAIL. can't box IntArrayRef and c10::optional<MemoryFormat>");
+  static_assert(can_box_all<const at::Tensor &, bool>::value, "FAIL. can't box const Tensor & and bool");
+
 template <class... OtherArgs>
 struct BoxedKernelWrapper<
   at::Tensor&(at::Tensor&, OtherArgs...),
@@ -213,10 +225,11 @@ struct BoxedKernelWrapper<
     KernelFunction::InternalBoxedKernelFunction* boxed_kernel_func,
     OperatorKernel* functor,
     const OperatorHandle& opHandle,
+    DispatchKeySet dispatchKeySet,
     at::Tensor& outArg, OtherArgs... otherArgs
   ) {
     torch::jit::Stack stack = boxArgs(outArg, otherArgs...);
-    (*boxed_kernel_func)(functor, opHandle, &stack);
+    (*boxed_kernel_func)(functor, opHandle, dispatchKeySet, &stack);
     TORCH_INTERNAL_ASSERT_DEBUG_ONLY(
       stack.size() == 1,
       "Boxed kernel was expected to return a single value on the stack, ",
@@ -250,10 +263,11 @@ struct BoxedKernelWrapper<
     KernelFunction::InternalBoxedKernelFunction* boxed_kernel_func,
     OperatorKernel* functor,
     const OperatorHandle& opHandle,
+    DispatchKeySet dispatchKeySet,
     FirstArg firstArg, RestArgs... restArgs
   ) {
     torch::jit::Stack stack = boxArgs(firstArg, restArgs...);
-    (*boxed_kernel_func)(functor, opHandle, &stack);
+    (*boxed_kernel_func)(functor, opHandle, dispatchKeySet, &stack);
     TORCH_INTERNAL_ASSERT_DEBUG_ONLY(
       stack.size() == 1,
       "Boxed kernel was expected to return a single value on the stack, ",
@@ -284,13 +298,14 @@ struct BoxedKernelWrapper<
     KernelFunction::InternalBoxedKernelFunction* boxed_kernel_func,
     OperatorKernel* functor,
     const OperatorHandle& opHandle,
+    DispatchKeySet dispatchKeySet,
     Args... args
   ) {
     using ArgTuple = std::tuple<Args...>;
     constexpr int RetCount = std::tuple_size<Result>();
 
     torch::jit::Stack stack = boxArgs(args...);
-    (*boxed_kernel_func)(functor, opHandle, &stack);
+    (*boxed_kernel_func)(functor, opHandle, dispatchKeySet, &stack);
     TORCH_INTERNAL_ASSERT_DEBUG_ONLY(
       stack.size() == RetCount,
       "Boxed kernel was expected to return ", RetCount, " values on the stack, ",
