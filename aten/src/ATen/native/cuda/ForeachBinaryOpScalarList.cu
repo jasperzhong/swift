6f49821c45f96be41673d0bcbf4dedcb2960d7f8
Iurii Zdebskyi <iuriiz@devfair004.maas>
2021-02-02 07:58:50 -0800

    Update foreach binary ops with a scalar list
    ghstack-source-id: 94fe411d3d70134484d138199f683e6cc06e786f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/49249

--- aten/src/ATen/native/cuda/ForeachBinaryOpScalarList.cu
+++ aten/src/ATen/native/cuda/ForeachBinaryOpScalarList.cu
@@ -1,6 +1,7 @@
 #include <ATen/Dispatch.h>
 #include <ATen/native/ForeachUtils.h>
 #include <ATen/native/cuda/ForeachFunctors.cuh>
+#include <ATen/native/BinaryOps.h>
 
 namespace at { namespace native {
 
@@ -16,7 +17,7 @@ std::vector<Tensor> foreach_binary_op(TensorList tensors, at::ArrayRef<double> s
     tensor_lists.emplace_back(tensors.vec());
     tensor_lists.emplace_back(vec_res);
 
-    AT_DISPATCH_ALL_TYPES_AND2(kBFloat16, kHalf, tensors[0].scalar_type(), "foreach_binary_op_scalarlist_cuda", [&]() {
+    AT_DISPATCH_ALL_TYPES_AND3(kBFloat16, kHalf, kBool, tensors[0].scalar_type(), "foreach_binary_op_scalarlist_cuda", [&]() {
         using opmath_t = get_opmath_t<scalar_t>::opmath_t;
         multi_tensor_apply<2>(tensor_lists,
                               scalars,
@@ -35,7 +36,7 @@ void foreach_binary_op_(TensorList tensors, at::ArrayRef<double> scalars) {
     std::vector<std::vector<at::Tensor>> tensor_lists;
     tensor_lists.emplace_back(tensors.vec());
 
-    AT_DISPATCH_ALL_TYPES_AND2(kBFloat16, kHalf, tensors[0].scalar_type(), "foreach_binary_op_scalarlist_cuda_", [&]() {
+    AT_DISPATCH_ALL_TYPES_AND3(kBFloat16, kHalf, kBool, tensors[0].scalar_type(), "foreach_binary_op_scalarlist_cuda_", [&]() {
         using opmath_t = get_opmath_t<scalar_t>::opmath_t;
         multi_tensor_apply<1>(tensor_lists,
                               scalars,
@@ -67,8 +68,49 @@ std::vector<Tensor> foreach_tensor_##NAME##_scalarlist_kernel_cuda(TensorList te
 }
 
 FOREACH_BINARY_OP_SCALARLIST(add, std::plus);
-FOREACH_BINARY_OP_SCALARLIST(sub, std::minus);
 FOREACH_BINARY_OP_SCALARLIST(mul, std::multiplies);
-FOREACH_BINARY_OP_SCALARLIST(div, std::divides);
+
+// In the case of division, integer inputs will result in float. 
+// Currently multi tensor apply can only return result of the same type as input.
+void foreach_tensor_div_scalarlist_kernel_cuda_(TensorList tensors, at::ArrayRef<Scalar> scalars) {
+    check_foreach_api_restrictions(tensors, scalars);
+    if (!can_use_fast_route(tensors, scalars, /*div_op*/ true)) {
+        return at::native::foreach_tensor_div_scalarlist_kernel_slow_(tensors, scalars);
+    }
+
+    foreach_binary_op_<std::divides>(tensors, scalars);
+}
+
+std::vector<Tensor> foreach_tensor_div_scalarlist_kernel_cuda(TensorList tensors, at::ArrayRef<Scalar> scalars) {
+    check_foreach_api_restrictions(tensors, scalars);
+    if (!can_use_fast_route(tensors, scalars, /*div_op*/ true)) {
+        return at::native::foreach_tensor_div_scalarlist_kernel_slow(tensors, scalars);
+    }
+
+    return foreach_binary_op<std::divides>(tensors, scalars);
+}
+
+// In the case of subtraction, we dont allow scalar to be boolean following the torch.sub logic
+void foreach_tensor_sub_scalarlist_kernel_cuda_(TensorList tensors, at::ArrayRef<Scalar> scalars) {
+    check_foreach_api_restrictions(tensors, scalars);
+    sub_check(tensors[0], scalars[0]);
+
+    if (!can_use_fast_route({tensors}, scalars)) {
+        return at::native::foreach_tensor_sub_scalarlist_kernel_slow_(tensors, scalars);
+    }
+
+    foreach_binary_op_<std::minus>(tensors, scalars);
+}
+
+std::vector<Tensor> foreach_tensor_sub_scalarlist_kernel_cuda(TensorList tensors, at::ArrayRef<Scalar> scalars) {
+    check_foreach_api_restrictions(tensors, scalars);
+    sub_check(tensors[0], scalars[0]);
+
+    if (!can_use_fast_route({tensors}, scalars)) {
+        return at::native::foreach_tensor_sub_scalarlist_kernel_slow(tensors, scalars);
+    }
+
+    return foreach_binary_op<std::minus>(tensors, scalars);
+}
 
 }} // namespace at::native
