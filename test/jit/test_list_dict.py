65fbd82500e1cbf18c21a3e2edc60d385eff2eae
Meghan Lele <meghanl@fb.com>
2021-02-04 14:43:41 -0800

    [WIP][JIT] Make and/or return the value of their operands
    **Summary**
    In Python, the value of the expression `a or b` is the value of the
    expression `a` if its boolean intepretation (e.g. `len(a) > 0` if it is
    a `list`) is `True`, and the value of the expression `b` if the boolean
    interpretation of `a` is `False`. `and` works in a similar way. In
    TorchScript, `and` and `or` always return boolean values, so it is not
    possible to write code like:
    
    ```
    @torch.jit.script
    def fn(a: List[int]):
      l = a or [1]
    ```
    
    where `l` is equal to `[1]` if `a` is empty, and `a` otherwise. This
    commit enables this feature in TorchScript. This is probably only useful
    for strings, but would still be a usability improvement overall.
    
    **Test Plan**
    This commit adds unit tests to `TestList` and `TestDict` to test this
    feature.
    
    ghstack-source-id: b340e9ea0fd96d28c1b77c7e7dacd0c219aeaa6e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/51684

--- test/jit/test_list_dict.py
+++ test/jit/test_list_dict.py
@@ -20,6 +20,27 @@ if __name__ == '__main__':
                        "instead.")
 
 class TestList(JitTestCase):
+    def test_list_logical_assignment(self):
+        def and_assignment(l: List[int]):
+            a = l and [3]
+            return a
+
+        self.checkScript(and_assignment, ([1, 2, 3],))
+        self.checkScript(and_assignment, ([],))
+
+        def or_assignment(l: List[int]):
+            a = l or [3]
+            return a
+
+        self.checkScript(or_assignment, ([1, 2, 3],))
+        self.checkScript(or_assignment, ([],))
+
+        def optional_assignment(l: Optional[List[int]]):
+            a = l or [3]
+            return a
+
+        self.checkScript(optional_assignment, (None,))
+
     def test_list_bool_conversion(self):
         def if_predicate(l: List[int]):
             if l:
@@ -1203,6 +1224,27 @@ class TestDict(JitTestCase):
 
     def dict_bool(self):
         return {True: 1}
+
+    def test_dict_logical_assignment(self):
+        def and_assignment(d: Dict[int, int]):
+            a = d and {10: 11}
+            return a
+
+        self.checkScript(and_assignment, ({1:2, 3:4},))
+        self.checkScript(and_assignment, ({},))
+
+        def or_assignment(d: Dict[int, int]):
+            a = d or {10: 11}
+            return a
+
+        self.checkScript(or_assignment, ({1:2, 3:4},))
+        self.checkScript(or_assignment, ({},))
+
+        def optional_assignment(d: Optional[Dict[int, int]]):
+            a = d or {10: 11}
+            return a
+
+        self.checkScript(optional_assignment, (None,))
 
     def test_dict_bool_conversion(self):
         def if_predicate(d: Dict[int, int]):
