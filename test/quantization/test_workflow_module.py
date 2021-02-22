529f7740bb982da85fad57b2af1402c9a16dc9cd
HDCharles <charlesdavidhernandez@gmail.com>
2021-02-21 22:52:42 -0800

    half and double precision fake quantization
    Summary: getting fp16 and fp64 models to work with fake quantization
    
    Test Plan: python test/test_quantization.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 2698662f8ecbaab0d3de641cdc2147e390de74c0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/52400
    
    Fake quantization support for half and double precision variables
    
    Summary: cpu forward works for double and half precision, backwards and gpu incomplete
    
    Test Plan:
    python test/test_quantization.py TestFakeQuantization.test_forward_half_precision_fake_quantize
    python test/test_quantization.py TestFakeQuantization.test_forward_double_precision_fake_quantize
    python test_test_quantization.py TestFakeQuantization.test_backward_double_precision_fake_quantize
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 2698662f8ecbaab0d3de641cdc2147e390de74c0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/52401
    
    Fake Quantize for Double and Half precision variables
    
    Summary: adding this so that I can rebase, otherwise integrated new tests with old test functions
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 2698662f8ecbaab0d3de641cdc2147e390de74c0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/52499
    
    Finished GPU and backward for fake quantization support for half and double precision floats
    
    Summary: added the data type macro to the cuda kernel and
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 2698662f8ecbaab0d3de641cdc2147e390de74c0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/52500
    
    Checking broken tests
    
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 2698662f8ecbaab0d3de641cdc2147e390de74c0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/52532
    
    Trying to consolidate diffs
    
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 2698662f8ecbaab0d3de641cdc2147e390de74c0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/52533
    
    Corrected linting and retested
    
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 2698662f8ecbaab0d3de641cdc2147e390de74c0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/52590

--- test/quantization/test_workflow_module.py
+++ test/quantization/test_workflow_module.py
@@ -850,7 +850,6 @@ class TestFakeQuantize(TestCase):
         X, (scale, zero_point, torch_type) = X
         quant_min = torch.iinfo(torch_type).min
         quant_max = torch.iinfo(torch_type).max
-
         X = to_tensor(X, device)
         X.requires_grad_()
         Y = _fake_quantize_per_tensor_affine_reference(X.cpu(), scale, zero_point, quant_min, quant_max)
@@ -863,21 +862,23 @@ class TestFakeQuantize(TestCase):
         np.testing.assert_allclose(dX.cpu(), X.grad.cpu().detach().numpy(), rtol=tolerance, atol=tolerance)
 
     def _test_forward_per_tensor_cachemask_impl(self, device):
-        for torch_type in (torch.qint8, torch.quint8):
-            Xs = (torch.randn(4, 8, device=device), torch.randn(4, 16, device=device)[:, ::2])
-            # pick the scale + zp so that some values get clipped
-            for X in Xs:
-                obs = torch.quantization.MinMaxObserver(torch_type)
-                obs(X * 0.75)
-                scale, zero_point = obs.calculate_qparams()
-                scale, zero_point = float(scale), int(zero_point)
-                quant_min, quant_max = obs._calculate_qmin_qmax()
+        for float_type in (torch.float32, torch.float16, torch.float64):
+            for torch_type in (torch.qint8, torch.quint8):
+                Xs = (torch.randn(4, 8, device=device), torch.randn(4, 16, device=device)[:, ::2])
+                # pick the scale + zp so that some values get clipped
+                for X in Xs:
+                    X = X.to(float_type)
+                    obs = torch.quantization.MinMaxObserver(torch_type)
+                    obs(X * 0.75)
+                    scale, zero_point = obs.calculate_qparams()
+                    scale, zero_point = float(scale), int(zero_point)
+                    quant_min, quant_max = obs._calculate_qmin_qmax()
 
-                Y_test = torch.fake_quantize_per_tensor_affine(
-                    X, scale, zero_point, quant_min, quant_max)
-                Y_ref = _fake_quantize_per_tensor_affine_reference(
-                    X.cpu(), scale, zero_point, quant_min, quant_max).to(device)
-                self.assertTrue(torch.allclose(Y_test, Y_ref, rtol=tolerance, atol=tolerance))
+                    Y_test = torch.fake_quantize_per_tensor_affine(
+                        X, scale, zero_point, quant_min, quant_max)
+                    Y_ref = _fake_quantize_per_tensor_affine_reference(  # torch.round doens't support half precision
+                        X.cpu().to(torch.float32), scale, zero_point, quant_min, quant_max).to(device).to(float_type)
+                    self.assertTrue(torch.allclose(Y_test, Y_ref, rtol=tolerance, atol=tolerance))
 
     def test_forward_per_tensor_cachemask_cpu(self):
         device = torch.device('cpu')
@@ -889,29 +890,30 @@ class TestFakeQuantize(TestCase):
         self._test_forward_per_tensor_cachemask_impl(device)
 
     def _test_backward_per_tensor_cachemask_impl(self, device):
-        for torch_type in (torch.qint8, torch.quint8):
-            X = torch.randn(4, 8).to(device)
-            X.requires_grad_()
-            # pick the scale + zp so that some values get clipped
-            obs = torch.quantization.MinMaxObserver(torch_type)
-            obs(X * 0.75)
-            scale, zero_point = obs.calculate_qparams()
-            scale, zero_point = float(scale), int(zero_point)
-            quant_min, quant_max = obs._calculate_qmin_qmax()
+        for float_type in (torch.float32, torch.float16, torch.float64):
+            for torch_type in (torch.qint8, torch.quint8):
+                X = torch.randn(4, 8).to(device).to(float_type)
+                X.requires_grad_()
+                # pick the scale + zp so that some values get clipped
+                obs = torch.quantization.MinMaxObserver(torch_type)
+                obs(X * 0.75)
+                scale, zero_point = obs.calculate_qparams()
+                scale, zero_point = float(scale), int(zero_point)
+                quant_min, quant_max = obs._calculate_qmin_qmax()
 
-            # forward pass
-            Y_test = torch.fake_quantize_per_tensor_affine(
-                X, scale, zero_point, quant_min, quant_max)
-            Y_ref = _fake_quantize_per_tensor_affine_reference(
-                X.cpu(), scale, zero_point, quant_min, quant_max).to(device)
-            self.assertTrue(torch.allclose(Y_test, Y_ref, rtol=tolerance, atol=tolerance))
+                # forward pass
+                Y_test = torch.fake_quantize_per_tensor_affine(
+                    X, scale, zero_point, quant_min, quant_max)
+                Y_ref = _fake_quantize_per_tensor_affine_reference(
+                    X.cpu().to(torch.float32), scale, zero_point, quant_min, quant_max).to(device).to(float_type)
+                self.assertTrue(torch.allclose(Y_test, Y_ref, rtol=tolerance, atol=tolerance))
 
-            # backward pass
-            dout = torch.rand(X.shape, dtype=torch.float).to(device)
-            dX = _fake_quantize_per_tensor_affine_grad_reference(
-                dout, X, scale, zero_point, quant_min, quant_max)
-            Y_test.backward(dout)
-            self.assertTrue(torch.allclose(dX, X.grad))
+                # backward pass
+                dout = torch.rand(X.shape, dtype=torch.float).to(device)
+                dX = _fake_quantize_per_tensor_affine_grad_reference(
+                    dout, X.to(torch.float32), scale, zero_point, quant_min, quant_max).to(float_type)
+                Y_test.backward(dout)
+                self.assertTrue(torch.allclose(dX, X.grad))
 
     def test_backward_per_tensor_cachemask_cpu(self):
         device = torch.device('cpu')
