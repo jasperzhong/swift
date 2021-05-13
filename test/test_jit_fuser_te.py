aed735c64a1a7c04221f7b497795cc25f6460711
Nikolay Korovaiko <korovaikon@gmail.com>
2021-05-13 17:54:05 +0000

    init hardshrink
    add to a fx test
    
    ghstack-source-id: bee37f22fc0317da33d10ef65671e966d5535a1a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/57749
    
    add aten_name
    
    forgot comma
    
    fix errors
    
    f16 types for cuda
    
    address feedback hardsrhink

--- test/test_jit_fuser_te.py
+++ test/test_jit_fuser_te.py
@@ -1441,6 +1441,7 @@ class TestTEFuser(JitTestCase):
             torch.round,
             torch.trunc,
             torch.frac,
+            F.hardshrink,
             lambda x: torch.threshold(x, 0, -10),
             lambda x: torch.clamp(x, -10, 10),
         ]
