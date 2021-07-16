e743cad2e4255c79b6c872b154637edd0254ea11
erjia <erjia@fb.com>
2021-07-16 19:59:05 +0000

    [DataLoader] Fix collate_fn for subclass of numpy object
    ghstack-source-id: 04b04078b877118231d8b6aa7503b60665c9f21d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/61769

--- torch/utils/data/_utils/collate.py
+++ torch/utils/data/_utils/collate.py
@@ -5,26 +5,28 @@ These **needs** to be in global scope since Py2 doesn't support serializing
 static methods.
 """
 
-import torch
-import re
 import collections
+try:
+    import numpy as np
+    HAS_NUMPY = True
+except ImportError:
+    HAS_NUMPY = False
+
 from torch._six import string_classes
 
-np_str_obj_array_pattern = re.compile(r'[SaUO]')
+import torch
 
 
 def default_convert(data):
     r"""Converts each NumPy array data field into a tensor"""
     elem_type = type(data)
+    if HAS_NUMPY:
+        if isinstance(data, np.ndarray):
+            return data
+        elif isinstance(data, (np.bool_, np.number)):
+            return torch.as_tensor(data)
     if isinstance(data, torch.Tensor):
         return data
-    elif elem_type.__module__ == 'numpy' and elem_type.__name__ != 'str_' \
-            and elem_type.__name__ != 'string_':
-        # array of string classes and object
-        if elem_type.__name__ == 'ndarray' \
-                and np_str_obj_array_pattern.search(data.dtype.str) is not None:
-            return data
-        return torch.as_tensor(data)
     elif isinstance(data, collections.abc.Mapping):
         return {key: default_convert(data[key]) for key in data}
     elif isinstance(data, tuple) and hasattr(data, '_fields'):  # namedtuple
@@ -45,6 +47,11 @@ def default_collate(batch):
 
     elem = batch[0]
     elem_type = type(elem)
+    if HAS_NUMPY:
+        if isinstance(elem, np.ndarray):
+            return default_collate([torch.as_tensor(b) for b in batch])
+        elif isinstance(elem, (np.bool_, np.number)):
+            return torch.as_tensor(batch)
     if isinstance(elem, torch.Tensor):
         out = None
         if torch.utils.data.get_worker_info() is not None:
@@ -54,16 +61,6 @@ def default_collate(batch):
             storage = elem.storage()._new_shared(numel)
             out = elem.new(storage)
         return torch.stack(batch, 0, out=out)
-    elif elem_type.__module__ == 'numpy' and elem_type.__name__ != 'str_' \
-            and elem_type.__name__ != 'string_':
-        if elem_type.__name__ == 'ndarray' or elem_type.__name__ == 'memmap':
-            # array of string classes and object
-            if np_str_obj_array_pattern.search(elem.dtype.str) is not None:
-                raise TypeError(default_collate_err_msg_format.format(elem.dtype))
-
-            return default_collate([torch.as_tensor(b) for b in batch])
-        elif elem.shape == ():  # scalars
-            return torch.as_tensor(batch)
     elif isinstance(elem, float):
         return torch.tensor(batch, dtype=torch.float64)
     elif isinstance(elem, int):
