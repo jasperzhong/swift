agcopenhaver/pytorch

implement "xy" indexing for torch.meshgrid

torch/testing/_internal/common_methods_invocations.py
@@ -3931,7 +3931,7 @@ def sample_inputs_meshgrid(
         ]
 
         sample_inputs = []
-        for shapes, indexing in itertools.product(test_cases, {'ij'}):
+        for shapes, indexing in itertools.product(test_cases, {'ij', 'xy'}):
             input, args = make_inputs(
                 [make_tensor(shape, device, dtype, requires_grad=requires_grad)
                  for shape in shapes])

jasperzhong/swift

torch/testing/_internal/common_methods_invocations.py
@@ -3986,7 +3986,7 @@ def sample_inputs_meshgrid(op_info: OpInfo, device: torch.device, dtype: torch.d
     ]
 
     sample_inputs = []
-    for shapes, indexing in itertools.product(test_cases, {'ij'}):
+    for shapes, indexing in itertools.product(test_cases, {'xy', 'ij'}):
         input, args = make_inputs(
             [make_tensor(shape, device, dtype, requires_grad=requires_grad)
              for shape in shapes])