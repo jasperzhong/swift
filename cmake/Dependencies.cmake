ecfb9ff50c6764f8e4fbebd3011e4654066c0e07
driazati <driazati@users.noreply.github.com>
2021-08-13 16:24:54 -0700

    cleanup

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1873,13 +1873,8 @@ list(APPEND Caffe2_DEPENDENCY_LIBS fmt::fmt-header-only)
 set(BUILD_SHARED_LIBS ${TEMP_BUILD_SHARED_LIBS} CACHE BOOL "Build shared libs" FORCE)
 
 if(USE_BREAKPAD)
-  message(STATUS "using")
   add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../third_party/breakpad)
-
-else()
-  message(STATUS "not using")
 endif()
-message(FATAL "stopped")
 
 # ---[ Kineto
 if(USE_KINETO AND INTERN_BUILD_MOBILE)
