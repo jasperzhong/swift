d982156f3eaecfcbc932916000ccbbb70b8d7354
vycezhong <izhongyuchen@gmail.com>
2021-12-01 20:23:21 +0800

    fix a bug

--- benchmarks/distributed/ft/pipeline_resnet50/run.sh
+++ benchmarks/distributed/ft/pipeline_resnet50/run.sh
@@ -18,6 +18,7 @@ cmd="python3 -m torch.distributed.run \
 	--global-batch-size 128 \
 	--seed 2021 \
 	-p 5 \
+	--logging \
 	~/data/ILSVRC2012" 
 
 OMP_NUM_THREADS=8 NCCL_IB_DISABLE=1 LOGLEVEL=DEBUG NCCL_DEBUG=INFO exec $cmd
