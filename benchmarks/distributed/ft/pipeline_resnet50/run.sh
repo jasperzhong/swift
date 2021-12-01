67231ad3393277a74a7415222cf2e441d61a92cd
vycezhong <izhongyuchen@gmail.com>
2021-12-01 18:17:29 +0800

    run plasma_store background

--- benchmarks/distributed/ft/pipeline_resnet50/run.sh
+++ benchmarks/distributed/ft/pipeline_resnet50/run.sh
@@ -6,7 +6,7 @@ MASTER_IP=10.28.1.16
 MASTER_PORT=1234
 export NCCL_SOCKET_IFNAME=eth2
 
-plasma_store -m 1000000000 -s /tmp/plasma
+nohup plasma_store -m 1000000000 -s /tmp/plasma &
 
 cmd="python3 -m torch.distributed.run \
 	--nnodes=$NNODES --nproc_per_node=$NPROC_PER_NODE \
