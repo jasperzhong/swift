f42ecc2ba35a0102762a5e44303d445a10f5180a
vycezhong <izhongyuchen@gmail.com>
2021-12-02 16:28:02 +0800

    flush objects to plasma in background

--- torch/distributed/fault_tolerance.py
+++ torch/distributed/fault_tolerance.py
@@ -1,5 +1,8 @@
 import functools
+import atexit
 import hashlib
+import threading
+from queue import Queue
 
 import pyarrow.plasma as plasma
 
@@ -25,6 +28,9 @@ def run(logging=False):
                 print(f"enable logging on device {torch.cuda.current_device()}")
                 distributed_c10d._logging_client = plasma.connect("/tmp/plasma")
                 distributed_c10d._logging_stream = torch.cuda.Stream()
+                distributed_c10d._logging_cpu_tensor_queue = Queue()
+                distributed_c10d._logging_thread = threading.Thread(target=distributed_c10d.flush_objects_to_plasma)
+                atexit.register(distributed_c10d._logging_thread.join)
 
             while True:
                 try:
