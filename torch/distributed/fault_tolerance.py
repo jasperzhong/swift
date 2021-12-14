1e554131914d88aa4fe3c9b5ec1674ec6157f2ce
vycezhong <izhongyuchen@gmail.com>
2021-12-14 13:01:00 +0000

    fix a bug

--- torch/distributed/fault_tolerance.py
+++ torch/distributed/fault_tolerance.py
@@ -18,7 +18,7 @@ from torch._C._distributed_c10d import SwiftInternalError
 from .data_parallel import (_DistributedOptimizer, broadcast_optimizer_state,
                             broadcast_parameters)
 from .distributed_c10d import (_failure_handler, all_gather, get_rank,
-                               get_world_size)
+                               get_world_size, get_local_world_size)
 
 try:
     import boto3
