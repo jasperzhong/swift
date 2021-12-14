070164d775b18dd5956901c1c4e0a341b77ff599
vycezhong <izhongyuchen@gmail.com>
2021-12-14 09:45:44 +0000

    print every 5 iterations

--- benchmarks/distributed/ft/pipeline_resnet50/run_aws.sh
+++ benchmarks/distributed/ft/pipeline_resnet50/run_aws.sh
@@ -13,6 +13,7 @@ cmd="python3 -m torch.distributed.run \
 	--micro-batch-size 16 \
 	--global-batch-size 128 \
 	--seed 2021 \
+	-p 5 \
 	~/data/ILSVRC2012" 
 
 OMP_NUM_THREADS=4 NCCL_IB_DISABLE=1 LOGLEVEL=DEBUG NCCL_DEBUG=INFO exec $cmd
