5eb6f9e5a4e912296737785d3f4ad5affd4bbd8c
vycezhong <izhongyuchen@gmail.com>
2021-12-21 12:29:47 +0000

    debug rng state

--- benchmarks/distributed/ft/pipeline_resnet50/schedule.py
+++ benchmarks/distributed/ft/pipeline_resnet50/schedule.py
@@ -84,8 +84,8 @@ def backward_step(input_tensor, output_tensor, output_tensor_grad):
         output_tensor_grad_checksum = 0 if output_tensor_grad is None else torch.sum(output_tensor_grad)
         input_tensor_grad_checksum = 0 if input_tensor_grad is None else torch.sum(input_tensor_grad)
         rng_state = torch.random.get_rng_state()
-        rng_state_str = ' '.join([str(ele.item()) for ele in rng_state])
-        f.write(f"{_cnt} {input_tensor_checksum} {output_tensor_checksum} {output_tensor_grad_checksum} {input_tensor_grad_checksum} rng: {rng_state_str}\n")
+        rng_state_checksum = torch.sum(rng_state.type(torch.float32))
+        f.write(f"{_cnt} {input_tensor_checksum} {output_tensor_checksum} {output_tensor_grad_checksum} {input_tensor_grad_checksum} {rng_state_checksum}\n")
         _cnt += 1
 
     return input_tensor_grad
