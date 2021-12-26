12aa915e82d0a26e1eb7b8d764baccca91990455
petersh6 <petershengwhu@gmail.com>
2021-12-26 17:31:19 +0800

    add run.sh

---
+++ benchmarks/distributed/ft/pipeline_bert/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NNODES=2
+NPROC_PER_NODE=2
+MASTER_IP=10.28.1.16
+MASTER_PORT=1234
+export NCCL_SOCKET_IFNAME=eth2
+
+cmd="python3 -m torch.distributed.run \
+	--nnodes=$NNODES --nproc_per_node=$NPROC_PER_NODE \
+	--rdzv_id=1234 --rdzv_backend=c10d \
+	--rdzv_endpoint=$MASTER_IP \
+	main.py \
+	--micro-batch-size 32 \
+	--global-batch-size 128 \
+	--seed 2021 \
+	-p 5 \
+	~/data/bert/train" 
+
+OMP_NUM_THREADS=8 NCCL_IB_DISABLE=1 LOGLEVEL=DEBUG NCCL_DEBUG=INFO exec $cmd
