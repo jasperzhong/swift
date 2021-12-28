bd3c720344901e8bb4f1e60727f839143d4a0b43
vycezhong <izhongyuchen@gmail.com>
2021-12-28 13:40:27 +0000

    debug

--- torch/distributed/fault_tolerance.py
+++ torch/distributed/fault_tolerance.py
@@ -265,9 +265,9 @@ def build_model_and_optimizer(config, model, optimizer, comm, failure_workers):
 
     # peer failure worker broadcast its parameters and optimizer states
     # to other group members
+    logger.info(f"Rank {peer_failure_worker} broadcast its parameters and optimizer states")
     broadcast_parameters(model.state_dict(), peer_failure_worker, comm_group=comm)
     broadcast_optimizer_state(optimizer, peer_failure_worker, comm_group=comm)
-    logger.info(f"Rank {peer_failure_worker} broadcast its parameters and optimizer states")
 
     return model, distributed_optimizer, peer_failure_worker
 
