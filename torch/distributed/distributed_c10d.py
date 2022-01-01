44badb9e438ddd46dbe2a9661c74b8a346654dd9
vycezhong <izhongyuchen@gmail.com>
2022-01-01 09:19:24 +0000

    fix a typo

--- torch/distributed/distributed_c10d.py
+++ torch/distributed/distributed_c10d.py
@@ -1107,7 +1107,7 @@ def recv(tensor,
     global _logging_rng_state_cnt
     global _logging_in_recovery
 
-    if _logging and _logging_in_recovery
+    if _logging and _logging_in_recovery:
         # on recovery
         if _logging_rng_state_fd is None:
             peer_failure_worker = get_rank() - _logging_group_diff
