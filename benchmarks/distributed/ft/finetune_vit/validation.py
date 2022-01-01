fad8d81d951c07dc4c43778a10b870ecf890aa11
petersh6 <petershengwhu@gmail.com>
2022-01-01 21:41:41 +0800

    update

--- benchmarks/distributed/ft/finetune_vit/validation.py
+++ benchmarks/distributed/ft/finetune_vit/validation.py
@@ -111,10 +111,8 @@ def forward_step(data_iterator, model, input_tensor, loss_func, loss):
         if is_pipeline_first_stage():
             images = images.cuda()
             images = transforms(images)
-            print(images.shape)
         elif is_pipeline_last_stage():
             labels = labels.cuda()
-            print(labels.shape)
 
     if is_pipeline_first_stage():
         assert input_tensor is None
@@ -124,8 +122,6 @@ def forward_step(data_iterator, model, input_tensor, loss_func, loss):
 
     if is_pipeline_last_stage():
         preds = output_tensor
-        print(labels.shape)
-        print(preds.shape)
         output_tensor = loss_func(output_tensor, labels)
         loss += output_tensor.item()
         
