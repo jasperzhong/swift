4f4e6ccb6f0b0c5a1ee15bd13c21023c2a94682c
petersh6 <petershengwhu@gmail.com>
2022-01-02 12:36:59 +0800

    fix a bug

--- benchmarks/distributed/ft/finetune_vit/validation.py
+++ benchmarks/distributed/ft/finetune_vit/validation.py
@@ -115,7 +115,7 @@ def forward(config, data_iterator, model, loss_func):
     
 def get_transform_func():
     transform = nn.Sequential(
-        transforms.RandomResizedCrop((224, 224), scale=(0.05, 1.0)),
+        transforms.Resize((224, 224), scale=(0.05, 1.0)),
         # ToTensor(transforms.ToTensor()),
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
     )
