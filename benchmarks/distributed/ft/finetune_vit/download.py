69200dab7e45481df2c7dbab0e814bd9a984ad25
vycezhong <izhongyuchen@gmail.com>
2022-01-02 11:18:40 +0000

    update

--- benchmarks/distributed/ft/finetune_vit/download.py
+++ benchmarks/distributed/ft/finetune_vit/download.py
@@ -1,27 +1,8 @@
-import torch
 from torchvision import datasets
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader, RandomSampler, DistributedSampler, SequentialSampler
-
-transform_train = transforms.Compose([
-        transforms.RandomResizedCrop((384, 384), scale=(0.05, 1.0)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-    ])
 
 trainset = datasets.CIFAR100(root="~/data/CIFAR100",
-                            train=True,
-                            download=False,
-                            transform=transform_train)
-
-train_sampler = RandomSampler(trainset)
-train_loader = DataLoader(trainset,
-                            sampler=train_sampler,
-                            batch_size=1,
-                            num_workers=4,
-                            pin_memory=True)
-train_iter = iter(train_loader)
-
-for i in range(1):
-    tensor = next(train_iter)
-    print(tensor[0].shape)
+                             train=True,
+                             download=True)
+testset = datasets.CIFAR100(root="~/data/CIFAR100",
+                            train=False,
+                            download=True)
