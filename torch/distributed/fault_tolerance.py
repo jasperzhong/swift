cfcb20cc0ffd4f87551482f28e736032cf10bb9d
petersh6 <petershengwhu@gmail.com>
2022-01-02 13:00:20 +0800

    print lr

--- torch/distributed/fault_tolerance.py
+++ torch/distributed/fault_tolerance.py
@@ -188,8 +188,8 @@ def fault_tolerance_train(config, train_iter, model, optimizer, data_loader, los
                         checksum(ts, model, optimizer)
 
                         if ts % config.print_freq == 0 and is_pipeline_last_stage():
-                            logger.info("[Iteration {}] loss: {:.6f} throughput: {:.2f} average iteration time: {}".format(
-                                ts, loss, config.batch_size / iteration_time, iter_time_avg / ts._value))
+                            logger.info("[Iteration {}] loss: {:.6f} throughput: {:.2f} average iteration time: {} lr: {}".format(
+                                ts, loss, config.batch_size / iteration_time, iter_time_avg / ts._value, lr_scheduler.get_lr()))
                     
                     break
                 except StopIteration as e:
