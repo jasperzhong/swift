7fd1fd4df21c9787799b5371d851c438df711749
vycezhong <izhongyuchen@gmail.com>
2022-01-04 08:36:50 +0000

    remove debug print

--- benchmarks/distributed/ft/pipeline_resnet50/main.py
+++ benchmarks/distributed/ft/pipeline_resnet50/main.py
@@ -137,48 +137,6 @@ def main():
     model = PipelineParallelResNet50(rank=args.rank, balance=[4, 2, 2, 3])
     model.cuda()
 
-    def hook_fn_forward(m, x, y):
-        def _dfs(tensors):
-            checksum = 0
-            if isinstance(tensors, tuple):
-                for t in tensors:
-                    checksum += _dfs(t)
-            elif isinstance(tensors, torch.cuda.FloatTensor):
-                checksum = torch.sum(tensors)
-            return checksum
-
-        input_checksum = _dfs(x)
-        output_checksum = _dfs(y)
-
-        with open("debug_forward_module.log", "a") as f:
-            f.write(f"{m._get_name()} {input_checksum} {output_checksum}\n")
-
-    def hook_fn_backward(m, grad_input, grad_output):
-        def _dfs(grad_input):
-            checksum = 0
-            if isinstance(grad_input, tuple):
-                for grad in grad_input:
-                    checksum += _dfs(grad)
-            elif isinstance(grad_input, torch.cuda.FloatTensor):
-                checksum = torch.sum(grad_input)
-            return checksum
-
-        grad_input_checksum = _dfs(grad_input)
-        grad_output_checksum = _dfs(grad_output)
-
-        with open("debug_backward_module.log", "a") as f:
-            f.write(f"{m._get_name()} {grad_input_checksum} {grad_output_checksum}\n")
-
-    def dfs(module):
-        for m in module.children():
-            if len(list(m.children())) == 0:
-                m.register_backward_hook(hook_fn_backward)
-                m.register_forward_hook(hook_fn_forward)
-            else:
-                dfs(m)
-
-    dfs(model.model_split)
-
     total_iters = args.benchmark_iters
     print("total iterations: {}".format(total_iters))
     num_micro_batches = args.global_batch_size // args.micro_batch_size
