c49b066a1f6eeacaee259d01506f515aaac0b801
petersh6 <petershengwhu@gmail.com>
2022-01-04 23:26:59 +0800

    use bertadam

--- benchmarks/distributed/ft/finetune_bert/main.py
+++ benchmarks/distributed/ft/finetune_bert/main.py
@@ -230,7 +230,7 @@ def main():
     print("total_iterations: {}".format(num_iterations))
      
     ## TODO: BERT Optimizer
-    optimizer = optim.Adam(model.parameters(), lr=5e-5, betas=(0.9, 0.999), weight_decay=0.01)
+    optimizer = optim.BertAdam(model.parameters(), lr=5e-5, betas=(0.9, 0.999), weight_decay=0.01)
     
     model.cuda()
     # TODO: choose which scheduler to use
