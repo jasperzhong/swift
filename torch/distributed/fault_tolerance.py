eca09702c383e6dcd414ad090d0d8c1bf6a4bb2d
vycezhong <izhongyuchen@gmail.com>
2022-01-05 09:16:41 +0000

    fix a bug

--- torch/distributed/fault_tolerance.py
+++ torch/distributed/fault_tolerance.py
@@ -160,6 +160,8 @@ def recovery(config, ts, model, optimizer, lr_scheduler=None):
         optimizer.clear()
         broadcast_optimizer_state(optimizer, pipeline_parallel_rank, comm_group=comm)
         logger.info(f"Rank {pipeline_parallel_rank} broadcast its parameters and optimizer states")
+        # for failure worker
+        ts = consensus_value
     elif config.logging:
         need_recovery = _need_recovery(config.groups, failure_workers)
         if need_recovery:
