6720d1822f6c5e376b3e17396e05efd7542f0cce
petersh6 <petershengwhu@gmail.com>
2022-01-06 12:18:18 +0800

    update

--- benchmarks/distributed/ft/pipeline_bert/run.sh
+++ benchmarks/distributed/ft/pipeline_bert/run.sh
@@ -6,6 +6,10 @@ MASTER_IP=10.28.1.27
 MASTER_PORT=1234
 export NCCL_SOCKET_IFNAME=enp94s0
 
+rm -rf *.h5
+rm -rf *.log
+rm -rf *.ckpt
+
 cmd="python3 -m torch.distributed.run \
 	--nnodes=$NNODES --nproc_per_node=$NPROC_PER_NODE \
 	--rdzv_id=1234 --rdzv_backend=c10d \
