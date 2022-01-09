c20786c20ae2361d36b2bf4766d47d18d5780fec
vycezhong <izhongyuchen@gmail.com>
2022-01-09 16:23:25 +0800

    pipeline bert global checkpoint failure free

--- torch/distributed/fault_tolerance.py
+++ torch/distributed/fault_tolerance.py
@@ -582,7 +582,7 @@ def fault_tolerance_train(config, train_iter, model, optimizer, data_loader, los
                                 f.write(f"{recovery_time}\n")
                         
                         # for experiment:
-                        if ts._value == 5 and get_rank() == 8 and not os.path.exists("./temp.flag"):
+                        if ts._value == 300 and get_rank() == 8 and not os.path.exists("./temp.flag"):
                             with open("temp.flag", "a") as f:
                                 f.write("Already killed\n")
                             os.system("ps aux | grep -i torch | grep -v grep | awk {'print $2'} | xargs kill -15")
