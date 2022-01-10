ce150e12375bb0ebe909bd11cf8a6cd535661cf9
vycezhong <izhongyuchen@gmail.com>
2022-01-10 18:14:39 +0800

    resnet

--- benchmarks/distributed/ft/pipeline_resnet50/main.py
+++ benchmarks/distributed/ft/pipeline_resnet50/main.py
@@ -140,6 +140,9 @@ def main():
 
     data_loader = get_data_loader(args)
     print(f"pipeline rank = {get_pipeline_model_parallel_rank()}")
+
+    balance = [4, 1, 1, 1, 2, 2]
+    # balance = [4, 1, 1, 1, 1, 3]
     model = PipelineParallelResNet50(rank=get_pipeline_model_parallel_rank(), balance=None)
     model.cuda()
 
