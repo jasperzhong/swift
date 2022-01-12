a494bcbd3e0a78dc3426d1f2d04b285f6ae0b6ee
vycezhong <izhongyuchen@gmail.com>
2022-01-12 16:41:14 +0800

    try partition

--- benchmarks/distributed/ft/pipeline_resnet50/run_bd.sh
+++ benchmarks/distributed/ft/pipeline_resnet50/run_bd.sh
@@ -19,7 +19,7 @@ cmd="python3 -m torch.distributed.run \
 	--rdzv_id=1234 --rdzv_backend=c10d \
 	--rdzv_endpoint=$MASTER_IP \
 	main.py \
-	--micro-batch-size 2048 \
+	--micro-batch-size 4096 \
 	--global-batch-size 16384 \
 	--benchmark-iters 200 \
 	--seed 42 \
