875dad0da6ac945036ecd94f904f1b0a18c613a2
vycezhong <izhongyuchen@gmail.com>
2022-01-12 18:05:06 +0800

    test

--- benchmarks/distributed/ft/pipeline_resnet50/run_bd.sh
+++ benchmarks/distributed/ft/pipeline_resnet50/run_bd.sh
@@ -19,8 +19,8 @@ cmd="python3 -m torch.distributed.run \
 	--rdzv_id=1234 --rdzv_backend=c10d \
 	--rdzv_endpoint=$MASTER_IP \
 	main.py \
-	--micro-batch-size 1024 \
-	--test-batch-size 2048 \
+	--micro-batch-size 2048 \
+	--test-batch-size 1024 \
 	--global-batch-size 16384 \
 	--benchmark-iters 200 \
 	--seed 42 \
