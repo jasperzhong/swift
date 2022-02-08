fbdc1cd1f8ca53c1380ed74e3e265c05d5456341
vycezhong <izhongyuchen@gmail.com>
2022-02-08 16:44:00 +0800

    update

--- benchmarks/distributed/ft/finetune_bert/run.sh
+++ benchmarks/distributed/ft/finetune_bert/run.sh
@@ -6,9 +6,14 @@ MASTER_IP=10.28.1.27
 MASTER_PORT=1234
 export NCCL_SOCKET_IFNAME=enp94s0
 
+ENABLE_LOGGING=${1:-0}
+LOGGING_GROUP_SIZE=${2:-${NPROC_PER_NODE}}
+PARALLEL_RECOVERY=${3:-0}
+
 rm -rf *.h5
 rm -rf *.log
 rm -rf *.ckpt
+hdfs dfs -rm -r "/*"
 
 cmd="python3 -m torch.distributed.run \
 	--nnodes=$NNODES --nproc_per_node=$NPROC_PER_NODE \
@@ -24,5 +29,19 @@ cmd="python3 -m torch.distributed.run \
 	/home/$USER/data/squad/v1.1/train-v1.1.json \
 	--predict-file /home/$USER/data/squad/v1.1/dev-v1.1.json
 	--vocab_file ./vocab"
+
+LOGGING_ARGS="
+	--logging \
+	--logging-dfs hdfs \
+	--logging-chunk-freq 5 \
+	--logging-group-size ${LOGGING_GROUP_SIZE}"
+
+if [[ $PARALLEL_RECOVERY -eq 1 ]]; then
+	LOGGING_ARGS="${LOGGING_ARGS} --parallel-recovery"
+fi
+
+if [[ $ENABLE_LOGGING -eq 1 ]];then
+	cmd="${cmd} ${LOGGING_ARGS}"
+fi
 
 OMP_NUM_THREADS=8 NCCL_IB_DISABLE=1 exec $cmd
