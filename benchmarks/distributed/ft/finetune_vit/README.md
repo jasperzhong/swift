74096388f40bdf02e9088ff04d6ced21b4559d66
vycezhong <izhongyuchen@gmail.com>
2022-04-20 11:19:22 +0800

    move benchmark scripts

--- benchmarks/distributed/ft/finetune_vit/README.md
+++
@@ -1,5 +0,0 @@
-## Setup
-
-```sh
-pip install -r requirements.txt --no-deps
-```
--- benchmarks/distributed/ft/finetune_vit/download.py
+++
@@ -1,8 +0,0 @@
-from torchvision import datasets
-
-trainset = datasets.CIFAR100(root="~/data/CIFAR100",
-                             train=True,
-                             download=True)
-testset = datasets.CIFAR100(root="~/data/CIFAR100",
-                            train=False,
-                            download=True)
